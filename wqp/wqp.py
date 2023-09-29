72a9aac1047936e47708b677fb3bb1fe6d77fc41
ayebaishmo <ishmo255super@gmail.com>
2023-09-29 12:17:30 +0300

    Intalled all libraries and modules and git ignored them

---
+++ wqp/wqp.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sb
+
+from sklearn.model_selection import train_tes_split
+from sklearn.preprocessing import MinMaxScaler
+from sklearn import metrics
+from sklearn.svm import SVC
+from sklearn.linear_model import LogisticRegression
