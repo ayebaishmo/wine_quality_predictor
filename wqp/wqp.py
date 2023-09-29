a69fe856f924652edac5261e1bfadd2b584bee6e
ayebaishmo <ishmo255super@gmail.com>
2023-09-29 19:32:06 +0300

    replaced null values with mean and displayed the data im histogram

--- wqp/wqp.py
+++ wqp/wqp.py
@@ -3,8 +3,29 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sb
 
-from sklearn.model_selection import train_tes_split
+from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
 from sklearn import metrics
+from xgboost import XGBClassifier
 from sklearn.svm import SVC
 from sklearn.linear_model import LogisticRegression
+
+import warnings
+warnings.filterwarnings('ignore')
+
+df = pd.read_csv('winequality.csv')
+#print(df.head)
+#df.info()
+#de = df.describe().T
+#print(de)
+#print(df.isnull().sum())
+
+for cole in df.columns:
+    if df[cole].isnull().sum() > 0:
+        df[cole] = df[cole].fillna(df[cole].mean())
+
+x = df.isnull().sum().sum()
+print(x)
+
+df.hist(bins=20, figsize=(10, 10))
+plt.show()
